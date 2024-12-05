d3cf52324f9f03ba3b94fe378b7e9a82d86a572f
Gabriel-Spinola <gabrielspinola77@gmail.com>
2024-12-05 10:36:21 -0300

    feat: test 2

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -11,6 +11,7 @@ export default async function Home({ params }: { params: { locale: string } }) {
   const translation = await getTranslation(params.locale as ImplLocale)
   const a = _Result.succeed("")
   console.log(denoShared())
+  //
 
   return (
     <>
