016c956165cd1b72bee25b57ce6d69a01b9d4079
Gabriel-Spinola <gabrielspinola77@gmail.com>
2024-12-05 11:39:52 -0300

    feat: test

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,9 +4,11 @@ import { getTranslation, ImplLocale } from './getTranslations'
 import Main from './home'
 import Header from '../_components/Header'
 import HeaderSkeleton from '../_components/skeletons/HomeSkeleton'
+import _Result from '@repo/common/result'
 
 export default async function Home({ params }: { params: { locale: string } }) {
   const translation = await getTranslation(params.locale as ImplLocale)
+  const result = _Result.succeed("")
 
   return (
     <>
